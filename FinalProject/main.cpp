685647d980aa896b246f30d53c55c48802528cd0
Rob Nolen <Rob Nolen>
2011-11-15 11:30:19 -0800

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Robert Nolen on 11/15/11.
+//  Copyright 2011 Greencrayon.org. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
