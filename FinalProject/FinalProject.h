1bf98c652b8a5a007fea751ebdc3cbd97b8efedf
Rob Nolen <rob.nolen@gmail.com>
2011-12-06 21:32:28 -0800

    add final project header

---
+++ FinalProject/FinalProject.h
@@ -0,0 +1,22 @@
+//
+//  FinalProject.h
+//  FinalProject
+//
+//  Created by Robert Nolen on 12/6/11.
+//  Copyright 2011 Greencrayon.org. All rights reserved.
+//
+
+#ifndef FinalProject_FinalProject_h
+#define FinalProject_FinalProject_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef std::string stdString;
+typedef std::vector<int> stdIntVector;
+typedef std::vector<int>::iterator stdIntVectorIterator;
+
+
+
+#endif
